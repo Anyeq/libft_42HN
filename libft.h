1e796422838d45054d6e0dd0852086e2b6ea5ab6
asando <alexando17@yahoo.com>
2025-03-17 13:15:11 +0100

    adding ft_lstadd_back ft_lstclear ft_lstdelone ft_lstlast modified libft.hw

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 07:45:53 by asando            #+#    #+#             */
-/*   Updated: 2025/03/16 21:30:04 by asando           ###   ########.fr       */
+/*   Updated: 2025/03/17 13:13:53 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef LIBFT_H
@@ -33,22 +33,22 @@ int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
 char	*ft_itoa(int n);
-//void	ft_lstadd_back(t_list **lst, t_list *new);
-//void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
 //void	ft_lstclear(t_list **lst, void (*del)(void *));
-//void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
 //void	ft_lstiter(t_list *lst, void (*f)(void *));
-//t_list	*ft_lstlast(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
 //t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
-//t_list	*ft_lstnew(void *content);
-//int		ft_lstsize(t_list *lst);
+t_list	*ft_lstnew(void *content);
+int		ft_lstsize(t_list *lst);
 void	*ft_memchr(const void *str, int c, size_t n);
 int		ft_memcmp(const char *s1, const char *s2, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dest, const void *src, size_t n);
 void	*ft_memset(void *str, int c, size_t n);
 void	ft_putchar_fd(char c, int fd);
-//void	ft_putendl_fd(char *s, int fd);
+void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 void	ft_putstr_fd(char *s, int fd);
 char	**ft_split(char const *s, char c);
